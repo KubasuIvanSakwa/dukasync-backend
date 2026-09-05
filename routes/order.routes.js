6be78b502d00ecd10f8bf8c76513433bbdd76499
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2026-09-05 13:29:55 +0300

    fix: getOrdersById

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import mongoose from "mongoose";
 import { authorize } from "../middleware/auth.middleware.js";
-import { createOrder } from "../controllers/order.controller.js";
+import { createOrder, getOrdersById } from "../controllers/order.controller.js";
 
 
 const orderRouter = Router()
